5e21d391d49d7b9e764ef5c91027323d9118c8b4
fnabiyevuz <fnabiyevuz@gmail.com>
2022-10-13 18:53:43 +0500

    admin.py is editted

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,17 @@ from django.contrib import admin
 from .models import *
 
 admin.site.register(Questions)
-admin.site.register(Answers)
-admin.site.register(Quizs)
 admin.site.register(AnswersOfParticipant)
+
+@admin.register(Answers)
+class AnswersAdmin(admin.ModelAdmin):
+    list_display = ('id', 'question', 'answer', 'status')
+    list_display_links = ('id', 'question',)
+
+
+@admin.register(Quizs)
+class QuizsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'participant', 'quantity', 'true', 'false', 'percentage', 'is_finished', 'date')
+    list_display_links = ('id', 'participant',)
+    list_filter = ('is_finished',)
+    date_hierarchy = 'date'
